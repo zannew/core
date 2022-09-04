db5191cb565a6b718ca1e0f1e535e51bf94847b1
“zannew“ <wonny21292@gmail.com>
2022-09-04 21:25:57 +0900

    DIP 적용하도록 수정(NPE 발생)

--- src/main/java/hellospring/core/order/OrderServiceImpl.java
+++ src/main/java/hellospring/core/order/OrderServiceImpl.java
@@ -9,7 +9,7 @@ import hellospring.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
