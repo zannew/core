1d6de8d68748e648b5aa9921999d9b55202a9d87
“zannew“ <wonny21292@gmail.com>
2022-09-18 23:49:58 +0900

    feature: 테스트 작성을 위한 빈 중복 등록 코드 추가

--- src/main/java/hellospring/core/AutoAppConfig.java
+++ src/main/java/hellospring/core/AutoAppConfig.java
@@ -1,14 +1,23 @@
 package hellospring.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+
+import hellospring.core.member.MemberRepository;
+import hellospring.core.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
 	// basePackages = "hellospring.core.member",
-	excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+	excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
+	@Bean(name = "memoryMemberRepository")
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
 }
