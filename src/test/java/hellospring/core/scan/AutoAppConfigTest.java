9c0787b1b945fe16b93d8e8c6121a10cfb4650ce
“zannew“ <wonny21292@gmail.com>
2022-09-19 22:58:32 +0900

    test: 의존관계 주입 확인 테스트 OrderService 추가

--- src/test/java/hellospring/core/scan/AutoAppConfigTest.java
+++ src/test/java/hellospring/core/scan/AutoAppConfigTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import hellospring.core.AutoAppConfig;
+import hellospring.core.member.MemberRepository;
 import hellospring.core.member.MemberService;
+import hellospring.core.order.OrderService;
+import hellospring.core.order.OrderServiceImpl;
 
 public class AutoAppConfigTest {
 
@@ -14,7 +17,12 @@ public class AutoAppConfigTest {
 	void basicScan() {
 		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 		MemberService memberService = ac.getBean(MemberService.class);
+		OrderServiceImpl orderService = ac.getBean(OrderServiceImpl.class);
 
 		assertThat(memberService).isInstanceOf(MemberService.class);
+		assertThat(orderService).isInstanceOf(OrderService.class);
+
+		MemberRepository memberRepository = orderService.getMemberRepository();
+		System.out.println("memberRepository = " + memberRepository);
 	}
 }
