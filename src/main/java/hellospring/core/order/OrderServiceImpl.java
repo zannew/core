5f1efef42856d3735750d07627817afb5f2e3d50
“zannew“ <wonny21292@gmail.com>
2022-09-20 23:31:39 +0900

    feature: 다양한 의존관계 주입 방식 (일부 주석 처리)

--- src/main/java/hellospring/core/order/OrderServiceImpl.java
+++ src/main/java/hellospring/core/order/OrderServiceImpl.java
@@ -10,20 +10,43 @@ import hellospring.core.member.MemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    // 1. 생성자 주입 방식
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // 2. setter 주입 방식
+    // private MemberRepository memberRepository;
+    // private DiscountPolicy discountPolicy;
+
+    // 3. 필드 주입 방식 (not recommended)
+    // @Autowired private MemberRepository memberRepository;
+    // @Autowired private DiscountPolicy discountPolicy;
+
+    // 1. 생성자 주입 방식
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("생성자 주입 방식");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    // 2. setter 주입 방식
+    // @Autowired
+    // public void setMemberRepository(MemberRepository memberRepository) {
+    //     System.out.println("setter 방식 -> memberRepository = " + memberRepository);
+    //     this.memberRepository = memberRepository;
+    // }
+
+    // @Autowired
+    // public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+    //     System.out.println("setter 방식 -> discountPolicy = " + discountPolicy);
+    //     this.discountPolicy = discountPolicy;
+    // }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
-
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
